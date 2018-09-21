4321e6601eb4e586738aa2198aa63c5ee68084a7
growdeuno <grow.deuno@gmail.com>
2018-09-22 01:18:06 +0800

    Reversed Bubble Sorting Algorithm

---
+++ reversed_bubble_sort.js
@@ -0,0 +1,23 @@
+function reverse_bubble_sort(arr){
+  var swap;
+  var n = arr.length-1;
+  var x = arr;
+
+  do{
+    swap = false;
+    for(i = 0; i < n; i++){
+      if(x[i] < x[i+1]){
+        var temp = x[i];
+        x[i] = x[i+1];
+        x[i+1] = temp;
+        swap = true;
+      }
+    }
+    n--;
+  }while(swap);
+  return x;
+}
+
+var arr = [8,4,3,2,1,5,9,7,6];
+console.log(arr);
+console.log(reverse_bubble_sort(arr));
